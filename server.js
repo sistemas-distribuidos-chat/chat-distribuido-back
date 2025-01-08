6862db94b282c7dac1d2e269b1fcc39f2b5b0ba3
Hudson <hudsoncdiniz@gmail.com>
2025-01-08 12:16:53 -0300

    update

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-const express = require('express');
-const connectDB = require('./database');
-const messageRoutes = require('./routes/messages');
-const authRoutes = require('./routes/auth');
+const express = require("express");
+const cors = require("cors"); // Importa o CORS
+const connectDB = require("./database");
+const messageRoutes = require("./routes/messages");
+const authRoutes = require("./routes/auth");
 
 const app = express();
-app.use(express.json()); // Middleware para parsear JSON
+
+// Middleware para habilitar o CORS
+app.use(
+  cors({
+    origin: "http://localhost:5173", // URL do frontend
+    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+    allowedHeaders: ["Content-Type", "Authorization"], // Cabeçalhos permitidos
+  })
+);
+
+// Middleware para parsear JSON
+app.use(express.json());
 
 // Conectar ao MongoDB
 connectDB();
 
 // Usar as rotas de mensagens
-app.use('/api', messageRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api", messageRoutes);
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
